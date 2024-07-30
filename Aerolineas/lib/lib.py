21924da548d34cbab126aa1ef97c8709721fb99f
GeorgiLezama <126032779+GeorgiLezama@users.noreply.github.com>
2024-07-29 20:29:18 -0600

    Update lib.py

--- Aerolineas/lib/lib.py
+++ Aerolineas/lib/lib.py
@@ -19,3 +19,27 @@ class lib:
         file = open("Data/data.json","w")
         json.dump(j,file,ensure_ascii=False,indent=4)
         file.close()
+
+# Tope de la pila en N
+    def registrar_carga(self,c,v):
+        file = open("Data/data.json","r")
+        j = json.load(file)
+        for i in range(0,len(j)):
+            if j[i]["Id"] == v.id:
+                if "Carga" not in j[i]:
+                    j[i]["Carga"] = []
+                j[i]["Carga"].append(c.toJson())
+        file = open("Data/data.json","w")
+        json.dump(j,file,ensure_ascii=False,indent=4)
+        file.close()
+
+    def descargar_en_carga(self,v):
+        file = open("Data/data.json","r")
+        j = json.load(file)
+        for i in range(0,len(j)):
+            if j[i]["Id"] == v.id:
+                j[i]["Carga"] = j[i]["Carga"][:-1]
+        file = open("Data/data.json","w")
+        json.dump(j,file,ensure_ascii=False,indent=4)
+        file.close()
+
